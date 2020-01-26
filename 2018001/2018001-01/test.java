f38483d3b7af8a215b5e7b2de2ed3a7e6229ad1f
Sachin <sachx2.0@hotmail.com>
2020-01-26 17:18:55 -0500

    sh

--- 2018001/2018001-01/test.java
+++ 2018001/2018001-01/test.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 public class test{
     public static void main(String arg[]){
         System.out.println("hello");
         System.out.println("fineman is a god");
+
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        if (name.length() == 3){
+            System.out.println("Short name");
+        } else {
+            System.out.println("Regular name");
+        }
     }
 }
