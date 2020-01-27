f1f231dd81ab74abbf7da1d95c6f9aa67acec167
Sachin <sachx2.0@hotmail.com>
2020-01-27 01:46:33 -0500

    shs

--- 2018001/2018001-01/test.java
+++ 2018001/2018001-01/test.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 public class test{
     public static void main(String arg[]){
-        System.out.println("hello");
-        System.out.println("fineman is a god");
 
         System.out.println("Enter a name");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
 
-        if (name.length() == 3){
+        int length = name.length();
+        System.out.println(length);
+
+        if (name.length() <= 3){
             System.out.println("Short name");
-        } else {
+        } else if (name.length() <= 6){
             System.out.println("Regular name");
+        } else {
+            System.out.println("fuckin massive name bruv");
         }
     }
 }
