ffe79197067dc3237ad935b03292df9207a3c27d
Sachin <sachx2.0@hotmail.com>
2020-09-19 13:05:49 -0400

    updates

---
+++ push.sh
@@ -0,0 +1,15 @@
+git add .
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+echo 'Enter the name of the branch:'
+read branch
+
+git push origin $branch
+
+read
+
+
